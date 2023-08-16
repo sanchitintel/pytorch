dbcc8dc428862d02c8a73c1df1227afb6ffa0d24
Wenzhe Xue <wenzhe.xue@intel.com>
2023-08-16 11:32:15 -0700

    remove unneccesary change

--- torch/_inductor/pattern_matcher.py
+++ torch/_inductor/pattern_matcher.py
@@ -1284,13 +1284,13 @@ def init_once_fakemode(fn: Callable[..., Any]):
 
     @functools.lru_cache(None)
     @functools.wraps(fn)
-    def lazy_init(*args, **kwargs):
+    def lazy_init():
         counters_ref = counters["inductor"].copy()
 
         with torch._guards.tracing(
             None
         ), maybe_disable_fake_tensor_mode(), FakeTensorMode():
-            result = fn(*args, **kwargs)
+            result = fn()
 
         # clear view matches encountered during tracing
         counters["inductor"] = counters_ref
